f1a2a44be37f89683ec817f71ade0b2e796d394a
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-08-03 15:54:46 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -310,16 +310,6 @@ resource "local_file" "save_private_key" {
   filename = "${var.key_path}${var.private_key_name}.pem"
 }
 
-data "template_file" "cloudmapper_script" {
-  template = "${file("cloudmapper_script.sh")}"
-  vars = {
-    cloudmapper_access_key = var.cloudmapper_access_key
-    cloudmapper_secret_key = var.cloudmapper_secret_key
-    aws_account_name = var.aws_account_name
-    aws_account_id = var.aws_account_id
-    aws_region = var.aws_region
-  }
-}
 
 
 
