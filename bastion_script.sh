f871f979ced7a63f689de991b7c78fe32f349a77
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-08-03 17:04:38 -0700

    Update bastion_script.sh

--- bastion_script.sh
+++ bastion_script.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+git clone https://github.com/mikegcoleman/react-wavelength-inference-demo.git
+
+cd react-wavelength-inference-demo && npm install
+
+npm run build
+
+cp -r ./build/* /home/bitnami/htdocs
+
